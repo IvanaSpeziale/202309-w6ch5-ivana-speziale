862d55347aadbdb46e59607bc749e5a726066bce
IvanaSpeziale <139582321+IvanaSpeziale@users.noreply.github.com>
2023-11-20 17:36:40 +0100

    Add render line

--- src/controller/trips.controller.ts
+++ src/controller/trips.controller.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from 'express';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 const dataFilePath = './api/db.json';
 let trips: any[] = [];
 
-try {
-  const rawData = fs.readFileSync(dataFilePath, 'utf-8');
+//MODIFICAR;
+/* try {
+  const rawData = fs.readFile(dataFilePath, 'utf-8');
   trips = JSON.parse(rawData).Trips || [];
 } catch (error) {
   console.error('Error al leer el archivo db.json:', error);
-}
+} */
 
 export const getAll = (_req: Request, res: Response) => {
   res.json(trips);
@@ -24,6 +25,8 @@ export const create = (req: Request, res: Response) => {
   const result = { ...req.body, id: trips.length + 1 };
   trips.push(result);
   res.json(result);
+
+  fs.writeFile(dataFilePath, trips);
 };
 
 export const update = (req: Request, res: Response) => {
@@ -31,7 +34,9 @@ export const update = (req: Request, res: Response) => {
   result = { ...result, ...req.body };
   trips[trips.findIndex((item) => item.id === Number(req.params.id))] = result!;
   res.json(result);
+  fs.writeFile(dataFilePath, trips);
 };
+// Ver repo de Andrea,if para definr error.
 
 export const remove = (req: Request, res: Response) => {
   trips.splice(
